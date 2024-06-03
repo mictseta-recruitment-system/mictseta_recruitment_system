79963060d509ce33247ab683d81d5325dc9279c0
Kgaogelo <131690193+ICTPass2002kgao@users.noreply.github.com>
2024-06-03 11:12:49 -0700

    static-files-updates

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 	path('sign_in/', views.sign_in, name="sign_in"),
 	path('log_out/',views.log_out, name="log_out"),
 	path('sign_up/', views.sign_up, name="sign_up"),
-	path('', views.landing, name="landing")
+	path('', views.Home, name="Home")
 ]
