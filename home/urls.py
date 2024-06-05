00002b82ce02ede1f24915695373b930f975f51b
Kgaogelo <131690193+ICTPass2002kgao@users.noreply.github.com>
2024-06-05 10:53:16 -0700

    about_us_page

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.contrib import admin
+from django.urls import path 
+
+urlpatterns = [ 
+    path('about_us/', views.about_us, name='about_us'),
+    path('contact_us/', views.contact_us, name='contact_us'),
+] 
--- mictseta_recruitment_system/urls.py
+++ mictseta_recruitment_system/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include("authenticate.urls")),
-    path('', include("authenticate.urls"))
+    path('', include("authenticate.urls")),
+    path('', include("home.urls"))
 ]
