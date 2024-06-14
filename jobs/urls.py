ae3b5e8a36c9c5ce164f844e0b40a8b0bf961f88
Adivhaho Jeff <221649921@edu.vut.ac.za>
2024-06-14 22:43:25 +0200

    added endpoints CRUD system: JobPost,Education, Skill, Experience, Reqirements

--- jobs/urls.py
+++ jobs/urls.py
@@ -10,8 +10,21 @@ urlpatterns = [
     path('add_job_acedemic/', views.add_job_acedemic, name='add_job_acedemic'),
     path('add_job_expereince/', views.add_job_expereince, name='add_job_expereince'),
     path('add_job_requirements/', views.add_job_requirements, name='add_job_requirements'),
-    path('get_jobs/', views.get_jobs, name='get_jobs')
-    
+    path('update_job/', views.update_job, name='update_job'),
+    path('update_job_skill/', views.update_job_skill, name='update_job_skill'),
+    path('update_job_acedemic/', views.update_job_acedemic, name='update_job_acedemic'),
+    path('update_job_expereince/', views.update_job_expereince, name='update_job_expereince'),
+    path('update_job_requirements/', views.update_job_requirements, name='update_job_requirements'),
+    path('delete_job/', views.delete_job, name='delete_job'),
+    path('delete_job_skill/', views.delete_job_skill, name='delete_job_skill'),
+    path('delete_job_acedemic/', views.delete_job_acedemic, name='delete_job_acedemic'),
+    path('delete_job_expereince/', views.delete_job_expereince, name='delete_job_expereince'),
+    path('delete_job_requirements/', views.delete_job_requirements, name='delete_job_requirements'),
+
+
+    path('get_jobs/', views.get_jobs, name='get_jobs'),
+
+
     # path('delete_user/', views.delete_user, name='delete_user')
 
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -164,9 +164,7 @@ def add_job_skill(request):
 			data = {
 				'level'			:	json_data.get('level'),
 				'name'	:	json_data.get('name'),
-				'job_post_id'	:	json_data.get('job_post_id')
-
-				
+				'job_post_id'	:	json_data.get('job_post_id')	
 			}
 			for key, value in data.items():
 				if key == None or value == None:
@@ -189,7 +187,7 @@ def add_job_skill(request):
 		else:
 			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
 	else:
-		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400).objects.filter(title=data['title'], industry=data['industry'], company_name=data['company_name']).exists()
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
 
 
 @csrf_protect
@@ -228,7 +226,7 @@ def add_job_acedemic(request):
 		else:
 			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
 	else:
-		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400).objects.filter(title=data['title'], industry=data['industry'], company_name=data['company_name']).exists()
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
 				
 
 @csrf_protect
@@ -265,7 +263,7 @@ def add_job_expereince(request):
 		else:
 			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
 	else:
-		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400).objects.create(user=request.user, title=data['title'],description=data['description'],location=data['location'], salary_range=data['salary_range'], job_type=data['job_type'], industry=data['industry'], company_name=data['company_name'], end_date=end_date)
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
 
 @csrf_protect				
 def add_job_requirements(request):
@@ -300,4 +298,376 @@ def add_job_requirements(request):
 		else:
 			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
 	else:
-		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400).objects.create(user=request.user, title=data['title'],description=data['description'],location=data['location'], salary_range=data['salary_range'], job_type=data['job_type'], industry=data['industry'], company_name=data['company_name'], end_date=end_date)
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
+
+#=============================================================================================================================================
+@csrf_protect
+def update_job(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			try:
+				json_data = json.loads(request.body)
+			except Exception:
+				return JsonResponse({'errors':'Supply a json oject: check documentation for more info ', 'status':'error'}, status=400)
+			data = {
+				'title'			:	json_data.get('title'),
+				'description'	:	json_data.get('description'),
+				'end_date'		:	json_data.get('end_date'),
+				'location'		:	json_data.get('location'),
+				'salary_range'	:	json_data.get('salary_range'),
+				'job_type'		:	json_data.get('job_type'),
+				'industry'		:	json_data.get('industry'),
+				'company_name'	:	json_data.get('company_name'),
+				'job_id'	:	json_data.get('job_id'),
+			}
+			for key, value in data.items():
+
+				if key == None or value == None:
+					return JsonResponse({'errors': {f'{key}':['this field is required ']}, 'status':'error'}, status=404)
+				if key in ['end_date']:
+					if " " in value:
+						return JsonResponse({'errors':{f'{key}':['spaces not allowed']}, 'status':'error'}, status=404)
+			try:
+				date_format = "%d:%b:%Y"
+				end_date = datetime.strptime(data['end_date'], date_format)
+			except:
+				return JsonResponse({'errors': 'Iconccerct data format try - DD:MMM:YYYY', 'status':'error'}, status=404)
+   
+			form = AddJobForm(data)
+			if form.is_valid():
+				exists = JobPost.objects.filter(id=int(data['job_id'])).exists()
+				if not exists:
+					return JsonResponse({'errors': {'job post':['Job does not exists']}, 'status': 'error'}, status=400)
+				try:
+					update_job_post = JobPost.objects.get(id=int(data['job_id']))
+					update_job_post.title = data['title']
+					update_job_post.description = data['description']
+					update_job_post.end_date = data['end_date']
+					update_job_post.location = data['location']
+					update_job_post.salary_range = data['salary_range']
+					update_job_post.job_type = data['job_type']
+					update_job_post.industry = data['industry']
+					update_job_post.company_name = data['company_name']
+					update_job_post.save()
+					return JsonResponse({'message': 'Job Post UPDATED Successfully', 'status': 'success'}, status=201)
+				except Exception as e:
+					return JsonResponse({"errors":{'server error':[f'{e}']}, "status":"error"}, status=400)
+			else:
+				return JsonResponse({"errors":form.errors, "status":"error"}, status=400)
+		else:
+			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
+	else:
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
+
+@csrf_protect
+def update_job_skill(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			try:
+				json_data = json.loads(request.body)
+			except Exception:
+				return JsonResponse({'errors':'Supply a json oject: check documentation for more info ', 'status':'error'}, status=400)
+			data = {
+				'level'			:	json_data.get('level'),
+				'name'	:	json_data.get('name'),
+				'job_skill_id'	:	json_data.get('job_skill_id')
+
+				
+			}
+			for key, value in data.items():
+				if key == None or value == None:
+					return JsonResponse({'errors': {f'{key}':['this field is required ']}, 'status':'error'}, status=404)
+   
+			form = AddJobSkillForm(data)
+			if form.is_valid():
+				exists = Skill.objects.filter(id=int(data['job_skill_id'])).exists()
+				if not exists:
+					return JsonResponse({'errors': {'job post':['Job Skills does not exist']}, 'status': 'error'}, status=400)
+				try:
+					job_skill = Skill.objects.get(id=int(data['job_skill_id']))
+					job_skill.name = data['name']
+					job_skill.level = data['level']
+					job_skill.save()
+					return JsonResponse({'message': 'Job skills updated Successfully', 'status': 'success'}, status=201)
+				except Exception as e:
+					return JsonResponse({"errors":{'server error':[f'{e}']}, "status":"error"}, status=400)
+			else:
+				return JsonResponse({"errors":form.errors, "status":"error"}, status=400)
+		else:
+			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
+	else:
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400).objects.filter(title=data['title'], industry=data['industry'], company_name=data['company_name']).exists()
+
+
+@csrf_protect
+def update_job_acedemic(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			try:
+				json_data = json.loads(request.body)
+			except Exception:
+				return JsonResponse({'errors':'Supply a json oject: check documentation for more info ', 'status':'error'}, status=400)
+			data = {
+				'level'			:	json_data.get('level'),
+				'qualification'	:	json_data.get('qualification'),
+				'job_academic_id'	:	json_data.get('job_academic_id')
+
+				
+			}
+			for key, value in data.items():
+				if key == None or value == None:
+					return JsonResponse({'errors': {f'{key}':['this field is required ']}, 'status':'error'}, status=404)
+   
+			form = AddJobAcademicForm(data)
+			if form.is_valid():
+				exists = Academic.objects.filter(id=int(data['job_academic_id'])).exists()
+				if not exists:
+					return JsonResponse({'errors': {'job post':['Job Adademic transcrript does not exist']}, 'status': 'error'}, status=400)
+				try:
+					job_academic = JobPost.objects.get(id=int(data['job_academic_id']))
+					job_academic.level = data['level']
+					job_academic.qualification = data['qualification'] 
+					job_academic.save()
+					return JsonResponse({'message': 'Job academic transcript updated Successfully', 'status': 'success'}, status=201)
+				except Exception as e:
+					return JsonResponse({"errors":{'server error':[f'{e}']}, "status":"error"}, status=400)
+			else:
+				return JsonResponse({"errors":form.errors, "status":"error"}, status=400)
+		else:
+			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
+	else:
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
+
+@csrf_protect
+def update_job_expereince(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			try:
+				json_data = json.loads(request.body)
+			except Exception:
+				return JsonResponse({'errors':'Supply a json oject: check documentation for more info ', 'status':'error'}, status=400)
+			data = {
+				'name'			:	json_data.get('name'),
+				'duration'	:	json_data.get('duration'),
+				'job_experience_id'	:	json_data.get('job_experience_id')
+			}
+			for key, value in data.items():
+				if key == None or value == None:
+					return JsonResponse({'errors': {f'{key}':['this field is required ']}, 'status':'error'}, status=404)
+
+			form = AddJobExperienceForm(data)
+			if form.is_valid():
+				exists = Experience.objects.filter(id=int(data['job_experience_id'])).exists()
+				if not exists:
+					return JsonResponse({'errors': {'job post':['Job Post does not exist']}, 'status': 'error'}, status=400)
+				try:
+					experience_post = Experience.objects.get(id=int(data['job_experience_id']))
+					experience_post.name = data['name']
+					experience_post.duration = data['duration']
+					add_job_experience_post.save()
+					return JsonResponse({'message': 'Job experience updated Successfully', 'status': 'success'}, status=201)
+				except Exception as e:
+					return JsonResponse({"errors":{'server error':[f'{e}']}, "status":"error"}, status=400)
+			else:
+				return JsonResponse({"errors":form.errors, "status":"error"}, status=400)
+		else:
+			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
+	else:
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
+
+@csrf_protect				
+def update_job_requirements(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			try:
+				json_data = json.loads(request.body)
+			except Exception:
+				return JsonResponse({'errors':'Supply a json oject: check documentation for more info ', 'status':'error'}, status=400)
+			data = {
+				'description'			:	json_data.get('description'),
+				'job_requirement_id'	:	json_data.get('job_requirement_id')
+			}
+			for key, value in data.items():
+				if key == None or value == None:
+					return JsonResponse({'errors': {f'{key}':['this field is required ']}, 'status':'error'}, status=404)
+
+			form = AddJobRequirementForm(data)
+			if form.is_valid():
+				exists = Requirement.objects.filter(id=int(data['job_requirement_id'])).exists()
+				if not exists:
+					return JsonResponse({'errors': {'job post':['Job Post does not exist']}, 'status': 'error'}, status=400)
+				try:
+					requirements_post = Requirement.objects.get(id=int(data['job_requirement_id']))
+					requirements_post.description = data['description']
+					requirements_post.save()
+					return JsonResponse({'message': 'Job requirements updated Successfully', 'status': 'success'}, status=201)
+				except Exception as e:
+					return JsonResponse({"errors":{'server error':[f'{e}']}, "status":"error"}, status=400)
+			else:
+				return JsonResponse({"errors":form.errors, "status":"error"}, status=400)
+		else:
+			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
+	else:
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
+
+
+#==============================================================================================================================================================================
+@csrf_protect
+def delete_job(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			try:
+				json_data = json.loads(request.body)
+			except Exception:
+				return JsonResponse({'errors':'Supply a json oject: check documentation for more info ', 'status':'error'}, status=400)
+			data = {
+				'job_id'	:	json_data.get('job_id'),
+			}
+			for key, value in data.items():
+
+				if key == None or value == None:
+					return JsonResponse({'errors': {f'{key}':['this field is required ']}, 'status':'error'}, status=404)
+				if key in ['end_date']:
+					if " " in value:
+						return JsonResponse({'errors':{f'{key}':['spaces not allowed']}, 'status':'error'}, status=404)
+   
+			
+			exists = JobPost.objects.filter(id=int(data['job_id'])).exists()
+			if not exists:
+				return JsonResponse({'errors': {'job post':['Job does not exists']}, 'status': 'error'}, status=400)
+			try:
+				update_job_post = JobPost.objects.get(id=int(data['job_id']))
+				update_job_post.delete()
+				update_job_post.save()
+				return JsonResponse({'message': 'Job Post removed Successfully', 'status': 'success'}, status=201)
+			except Exception as e:
+				return JsonResponse({"errors":{'server error':[f'{e}']}, "status":"error"}, status=400)
+		else:
+			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
+	else:
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
+
+@csrf_protect
+def delete_job_skill(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			try:
+				json_data = json.loads(request.body)
+			except Exception:
+				return JsonResponse({'errors':'Supply a json oject: check documentation for more info ', 'status':'error'}, status=400)
+			data = {
+				'job_skill_id'	:	json_data.get('job_skill_id')	
+			}
+			for key, value in data.items():
+				if key == None or value == None:
+					return JsonResponse({'errors': {f'{key}':['this field is required ']}, 'status':'error'}, status=404)
+   
+			
+			exists = Skill.objects.filter(id=int(data['job_skill_id'])).exists()
+			if not exists:
+				return JsonResponse({'errors': {'job post':['Job Skills does not exist']}, 'status': 'error'}, status=400)
+			try:
+				job_skill = Skill.objects.get(id=int(data['job_skill_id']))
+				job_skill.delete()
+				job_skill.save()
+				return JsonResponse({'message': 'Job skills remved Successfully', 'status': 'success'}, status=201)
+			except Exception as e:
+				return JsonResponse({"errors":{'server error':[f'{e}']}, "status":"error"}, status=400)
+			
+		else:
+			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
+	else:
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400).objects.filter(title=data['title'], industry=data['industry'], company_name=data['company_name']).exists()
+
+@csrf_protect
+def delete_job_acedemic(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			try:
+				json_data = json.loads(request.body)
+			except Exception:
+				return JsonResponse({'errors':'Supply a json oject: check documentation for more info ', 'status':'error'}, status=400)
+			data = {
+				'job_academic_id'	:	json_data.get('job_academic_id')
+			}
+			for key, value in data.items():
+				if key == None or value == None:
+					return JsonResponse({'errors': {f'{key}':['this field is required ']}, 'status':'error'}, status=404)
+			
+			exists = Academic.objects.filter(id=int(data['job_academic_id'])).exists()
+			if not exists:
+				return JsonResponse({'errors': {'job post':['Job Adademic transcrript does not exist']}, 'status': 'error'}, status=400)
+			try:
+				job_academic = JobPost.objects.get(id=int(data['job_academic_id']))
+				job_academic.delete()
+				job_academic.save()
+				return JsonResponse({'message': 'Job academic transcript updated Successfully', 'status': 'success'}, status=201)
+			except Exception as e:
+				return JsonResponse({"errors":{'server error':[f'{e}']}, "status":"error"}, status=400)
+			
+		else:
+			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
+	else:
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
+
+@csrf_protect
+def delete_job_expereince(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			try:
+				json_data = json.loads(request.body)
+			except Exception:
+				return JsonResponse({'errors':'Supply a json oject: check documentation for more info ', 'status':'error'}, status=400)
+			data = {
+				'job_experience_id'	:	json_data.get('job_experience_id')
+			}
+			for key, value in data.items():
+				if key == None or value == None:
+					return JsonResponse({'errors': {f'{key}':['this field is required ']}, 'status':'error'}, status=404)
+	
+			exists = Experience.objects.filter(id=int(data['job_experience_id'])).exists()
+			if not exists:
+				return JsonResponse({'errors': {'job post':['Job Post does not exist']}, 'status': 'error'}, status=400)
+			try:
+				experience_post = Experience.objects.get(id=int(data['job_experience_id']))
+				experience_post.delete()
+				add_job_experience_post.save()
+				return JsonResponse({'message': 'Job experience updated Successfully', 'status': 'success'}, status=201)
+			except Exception as e:
+				return JsonResponse({"errors":{'server error':[f'{e}']}, "status":"error"}, status=400)
+			
+		else:
+			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
+	else:
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
+
+@csrf_protect				
+def delete_job_requirements(request):
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			try:
+				json_data = json.loads(request.body)
+			except Exception:
+				return JsonResponse({'errors':'Supply a json oject: check documentation for more info ', 'status':'error'}, status=400)
+			data = {
+				'job_requirement_id'	:	json_data.get('job_requirement_id')
+			}
+			for key, value in data.items():
+				if key == None or value == None:
+					return JsonResponse({'errors': {f'{key}':['this field is required ']}, 'status':'error'}, status=404)
+	
+			exists = Requirement.objects.filter(id=int(data['job_requirement_id'])).exists()
+			if not exists:
+				return JsonResponse({'errors': {'job post':['Job Post does not exist']}, 'status': 'error'}, status=400)
+			try:
+				requirements_post = Requirement.objects.get(id=int(data['job_requirement_id']))
+				requirements_post.description = data['description']
+				requirements_post.save()
+				return JsonResponse({'message': 'Job requirements updated Successfully', 'status': 'success'}, status=201)
+			except Exception as e:
+				return JsonResponse({"errors":{'server error':[f'{e}']}, "status":"error"}, status=400)
+			
+		else:
+			return JsonResponse({'errors': {'method':['Invalid request method']}, 'status': 'error'}, status=400)
+	else:
+		return JsonResponse({'errors': {'authentication' : ['you are not logged in']}, 'status': 'error'}, status=400)
