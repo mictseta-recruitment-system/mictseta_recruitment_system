da920d2a2bae5f77512c09b97468005ece1674bc
Adivhaho Jeff <221649921@edu.vut.ac.za>
2024-06-22 13:28:41 +0200

    path

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,7 @@ from django.urls import path
 urlpatterns = [ 
     path('about_us/', views.about_us, name='about_us'),
     path('contact_us/', views.contact_us, name='contact_us'),
+    path('job_details/', views.job_details, name='job_details'),
+    
     path('', views.home, name='home')
 ] 
