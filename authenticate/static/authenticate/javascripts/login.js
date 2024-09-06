087a17b86cab1d2f313e0e3857ce962cb00dc393
Kgaogelo <kgaogelojoseph33@gmail.com>
2024-09-05 18:20:11 -0700

    updated-the-login-form

--- authenticate/static/authenticate/javascripts/login.js
+++ authenticate/static/authenticate/javascripts/login.js
@@ -1,199 +1,208 @@
+function handle_sign_in_button_click() {
+  const lemail = document.getElementById("lemail").value;
+  const lpassword = document.getElementById("lpassword").value;
 
-    
-    function handle_sign_in_button_click() {
-        const lemail = document.getElementById("lemail").value;
-        const lpassword = document.getElementById("lpassword").value;
-        
-       /* const forms = document.getElementById("login-form");
+  /* const forms = document.getElementById("login-form");
         csrftoken = forms.getElementsByTagName("input")[0].value;
         console.log(csrftoken)*/
-        const data1 = {
-          email: lemail,
-          password: lpassword,
-          csrfmiddlewaretoken:getCookie('csrftoken') ,
-          /*csrftoken:csrftoken,
+
+  document.getElementById("flash-message-container").innerHTML = ``;
+  if (lemail == "" && !lpassword == "") {
+    showFlashMessage("All the fields are required!", "danger");
+    return;
+  }
+
+  const data1 = {
+    email: lemail,
+    password: lpassword,
+    csrfmiddlewaretoken: getCookie("csrftoken"),
+    /*csrftoken:csrftoken,
           jeff:"jeff"*/
-        };
-        fetch("http://127.0.0.1:8000/auth/sign_in/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            'X-CSRFToken':getCookie('csrftoken') ,
-            'csrfmiddlewaretoken':getCookie('csrftoken') ,
-            'csrftoken':getCookie('csrftoken')
-          },
-          body: JSON.stringify(data1),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.status === "error") {
-              if (data.errors) {
-                handleErrors(data.errors);
-              } else {
-                showFlashMessage("An unknown error occurred", "danger");
-              }
-            } else if (data.status === "success") {
-                    if(data.user_type ==="staff"){
-                      showFlashMessage(data.message, "success");
-                      window.location.href = "/dashboard";
-                    }else{
-                      showFlashMessage(data.message, "success");
-                       window.location.href = "/";
-                    }
+  };
+  fetch("http://127.0.0.1:8000/auth/sign_in/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": getCookie("csrftoken"),
+      csrfmiddlewaretoken: getCookie("csrftoken"),
+      csrftoken: getCookie("csrftoken"),
+    },
+    body: JSON.stringify(data1),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "error") {
+        if (data.errors) {
+          handleErrors(data.errors);
+        } else {
+          showFlashMessage("An unknown error occurred", "danger");
+        }
+      } else if (data.status === "success") {
+        if (data.user_type === "staff") {
+          showFlashMessage(data.message, "success");
+          window.location.href = "/dashboard";
+        } else {
+          showFlashMessage(data.message, "success");
+          window.location.href = "/";
+        }
+      } else if (data.status === "warning") {
+        showFlashMessage(data.message, "warning");
+        window.location.href = "/";
+        showFlashMessage(data.message, "warning");
+      }
+    })
+    .catch((error) => {
+      showFlashMessage("An unexpected error occurred", "danger");
+      console.error("Error:", error);
+    });
+}
 
-                
+/*===================================================================================*/
 
-            } else if (data.status === "warning") {
-                showFlashMessage(data.message, "warning");
-                window.location.href = "/";
-                showFlashMessage(data.message, "warning");
-            }
-          })
-          .catch((error) => {
-            showFlashMessage("An unexpected error occurred", "danger");
-            console.error("Error:", error);
-          });
-    }
-   
-
-       /*===================================================================================*/
-
-
-    function handle_sign_up_button_click() {
-      /*  const first_name = document.getElementById("first_name").value;
+function handle_sign_up_button_click() {
+  /*  const first_name = document.getElementById("first_name").value;
         const last_name = document.getElementById("last_name").value;*/
-        
-        const email = document.getElementById("email").value;
-        const idnumber = document.getElementById("idnumber").value;
-        const password = document.getElementById("password").value;
-        const password2 = document.getElementById("password2").value;
-      /*  const phone = document.getElementById("phone").value;*/
 
+  const email = document.getElementById("email").value;
+  const idnumber = document.getElementById("idnumber").value;
+  const password = document.getElementById("password").value;
+  const password2 = document.getElementById("password2").value;
+  /*  const phone = document.getElementById("phone").value;*/
+  document.getElementById("flash-message-container").innerHTML = ``;
+  if (email == "" && password == "" && idnumber == "" && password == "") {
+    showFlashMessage("All the fields are required!", "danger");
+    return;
+  }
+  if (password != password2) {
+    showFlashMessage("The passwords does not match!", "danger");
+    return;
+  }
+  if (idnumber.length < 13) {
+    showFlashMessage(
+      "The ID number is invalid, Please provide SA ID!",
+      "danger"
+    );
+    return;
+  }
 
-        const data2 = {
-            /*first_name : first_name,
+  const data2 = {
+    /*first_name : first_name,
             last_name : last_name,*/
-           /* phone :  phone , */ 
-            email : email,
-           
-            idnumber : idnumber,
-            password : password,
-            password2 : password2
-        };
+    /* phone :  phone , */
+    email: email,
 
-        fetch("http://127.0.0.1:8000/auth/sign_up/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            'X-CSRFToken': getCookie('csrftoken') 
-          },
-          body: JSON.stringify(data2),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.status === "error") {
-              if (data.errors) {
-                handleErrors(data.errors);
-              } else {
-                showFlashMessage("An unknown error occurred", "danger");
-              }
-            } else if (data.status === "success") {
-                window.location.href = "/auth/authenticates/";
-                showFlashMessage(data.message, "success");
+    idnumber: idnumber,
+    password: password,
+    password2: password2,
+  };
 
-            } else if (data.status === "warning") {
-                window.location.href = "/auth/authenticates/";
-                showFlashMessage(data.message, "warning");
-            }
-          })
-          .catch((error) => {
-            showFlashMessage("An unexpected error occurred", "danger");
-          });
-    }
+  fetch("http://127.0.0.1:8000/auth/sign_up/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": getCookie("csrftoken"),
+    },
+    body: JSON.stringify(data2),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "error") {
+        if (data.errors) {
+          handleErrors(data.errors);
+        } else {
+          showFlashMessage("An unknown error occurred", "danger");
+        }
+      } else if (data.status === "success") {
+        window.location.href = "/auth/authenticates/";
+        showFlashMessage(data.message, "success");
+      } else if (data.status === "warning") {
+        window.location.href = "/auth/authenticates/";
+        showFlashMessage(data.message, "warning");
+      }
+    })
+    .catch((error) => {
+      showFlashMessage("An unexpected error occurred", "danger");
+    });
+}
 
-    /*===================================================================================*/ 
+/*===================================================================================*/
 
-     
-    /*===================================================================================*/ 
+/*===================================================================================*/
 
+function handle_forgot_button_click() {
+  const femail = document.getElementById("femail").value;
 
-
-
-    function handle_forgot_button_click() {
-        const femail = document.getElementById("femail").value;
-        
-
-        const data1 = {
-          email: femail,
-        };
-        fetch("http://127.0.0.1:8000/auth/reset_password_link/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            'X-CSRFToken': getCookie('csrftoken'),
-          },
-          body: JSON.stringify(data1),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.status === "error") {
-              if (data.errors) {
-                handleErrors(data.errors);
-              } else {
-                showFlashMessage("An unknown error occurred", "danger");
-              }
-            } else if (data.status === "success") {
-                showFlashMessage(data.message, "success");
-                localStorage.setItem('value', data.link);
-                window.location.href = "/auth/reset_link/";
-
-            } else if (data.status === "warning") {
-                showFlashMessage(data.message, "warning");
-                window.location.href = "";
-                showFlashMessage(data.message, "warning");
-            }
-          })
-          .catch((error) => {
-            showFlashMessage("An unexpected error occurred", "danger");
-          });
-    }
-   
-
-
-
-
+  const data1 = {
+    email: femail,
+  };
+  fetch("http://127.0.0.1:8000/auth/reset_password_link/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": getCookie("csrftoken"),
+    },
+    body: JSON.stringify(data1),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "error") {
+        if (data.errors) {
+          handleErrors(data.errors);
+        } else {
+          showFlashMessage("An unknown error occurred", "danger");
+        }
+      } else if (data.status === "success") {
+        showFlashMessage(data.message, "success");
+        localStorage.setItem("value", data.link);
+        window.location.href = "/auth/reset_link/";
+      } else if (data.status === "warning") {
+        showFlashMessage(data.message, "warning");
+        window.location.href = "";
+        showFlashMessage(data.message, "warning");
+      }
+    })
+    .catch((error) => {
+      showFlashMessage("An unexpected error occurred", "danger");
+    });
+}
 
 function handleErrors(errors) {
   for (const key in errors) {
-    //to ensure it makes the container Empty before displaying the next error 
-document.getElementById('flash-message-container').innerHTML=``;
-//the next error
+    //to ensure it makes the container Empty before displaying the next error
+    document.getElementById("flash-message-container").innerHTML = ``;
+    //the next error
     if (errors.hasOwnProperty(key)) {
       const error = errors[key];
       if (Array.isArray(error)) {
         error.forEach((errorMessage) => {
-          showFlashMessage(`${key}:${errorMessage}`, "danger");
+          if (key == "email") {
+            showFlashMessage(`Email is not registered`, "danger");
+          } else if (key == "password") {
+            document.getElementById("flash-message-container").innerHTML = ``;
+            showFlashMessage(`Incorrect credentials`, "danger");
+          }
         });
       } else {
-        showFlashMessage(`${key}: ${error}`, "danger");
+        document.getElementById("flash-message-container").innerHTML = ``;
+        showFlashMessage(`${error}`, "danger");
       }
     }
   }
 }
 
 function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 
 var btnSignUp = document.getElementById("btn-sign-up");
--- rest_api/urls.py
+++ rest_api/urls.py
@@ -4,9 +4,9 @@ from . import views
  
 
 urlpatterns = [
-    path('auth/sign-in/',views.sign_in,name="sign_in"),
-    path('auth/sign-up/',views.sign_up,name="sign_up"),
-    path('auth/log-out/',views.log_out,name="log_out"),
+    path('auth/sign-in/',views.sign_in,name="api_sign_in"),
+    path('auth/sign-up/',views.sign_up,name="api_sign_up"),
+    path('auth/log-out/',views.log_out,name="api_log_out"),
     path('auth/reset-password/<uidb64>/<token>/',views.reset_password,name="reset_password"),
     path('jobs/', views.get_all_jobs, name='get_all_jobs'),
     path('jobs/apply/<int:jobID>/', views.apply_for_job, name='apply_for_job'),
