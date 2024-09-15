7fcf5ce7e834bd4b4143df4be360ae192f0c45e5
ethanux <221649921@edu.vut.ac.za>
2024-09-15 14:50:54 +0200

    Test deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py collectstatic --noinput
+web: gunicorn mictseta_recruitment_system.wsgi --log-file -
--- mictseta_recruitment_system/settings.py
+++ mictseta_recruitment_system/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -217,12 +218,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
