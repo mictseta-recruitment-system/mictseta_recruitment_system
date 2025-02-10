a58f9f7386c29249ab51f5bbb6636100f255eaf4
six skies <221649921@edu.vut.ac.za>
2025-02-10 21:10:24 +0200

    ai chtbot

--- AI/urls.py
+++ AI/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 urlpatterns = [ 
     path('train_model/', views.train_model, name='train_model'),
  	path('chat/', views.chat_page, name='chat'),
- 	path('test_model/',views.test_model, name="test_model")
+ 	path('chat_live/',views.chat_live, name="chat_live")
 
  	]
